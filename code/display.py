1a249e9a1f182874fc8589fb5e7207888aecbfb5
Keerat <keerat2012@gmail.com>
2022-04-25 14:15:29 -0500

    finished visualization code

--- code/display.py
+++ code/display.py
@@ -2,19 +2,25 @@
 # graph. To use this you will need to install plotly and pandas
 # pip install plotly
 # pip install pandas
-# the script does not currently deal with timestamp overflow
+# the script does not currently deal with timestamp overflow, because
+# millis starts at zero when the arduino is powered and does not roll over
+# until apporx 50 days have passed, which is like a whole bee season
 # the graph will display on your default browser
+# the script also clears the file in which the arduino writes data, and makes a
+# copy of the data it cleared in a file named with the data and time
 
 import csv
 import plotly.express as px
+import shutil
+import datetime
 
-#lists to store data from the text file
+# lists to store data from the text file
 time=[]
 sen1=[]
 sen2=[]
 sen3=[]
 
-#populate lists with data from csv file
+# populate lists with data from csv file
 with open("D:\SEN3.TXT") as File:
   beedata = csv.reader(File, delimiter = ',')
 
@@ -36,3 +42,15 @@ fig.update_layout(
     )
 
 fig.show()
+
+#make a correctly dated copy of data
+now = str(datetime.datetime.now())[5:19]
+now = now.replace(":","_")
+
+src="D:\SEN3.TXT"
+dst="D:data"+str(now)+".txt"
+shutil.copyfile(src,dst)
+
+#clear the original file to make space for next reading
+with open("D:\SEN3.TXT", 'w'):
+    pass
